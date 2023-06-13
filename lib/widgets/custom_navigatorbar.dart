8008d6ae28a111b3a9ced22bff096f0bf18798c6
Elianyi <elianyi.otano@4geeks.io>
2023-06-13 12:32:34 -0400

    (LE-8): Set Bottombar

--- lib/widgets/custom_navigatorbar.dart
+++ lib/widgets/custom_navigatorbar.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 
-class CustomNavigationBar extends StatelessWidget {
+class CustomNavigationBar extends StatefulWidget {
+  @override
+  _CustomNavigationBarState createState() => _CustomNavigationBarState();
+}
+
+class _CustomNavigationBarState extends State<CustomNavigationBar> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      currentIndex: 1,
-      elevation: 0,
+      onTap: (index) {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+      currentIndex: _currentIndex,
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: Color.fromRGBO(49, 220, 118, 1),
+      unselectedItemColor: Colors.black,
       items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
         BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_month_outlined), label: 'Events'),
+          icon: Icon(Icons.home_outlined),
+          label: 'Home',
+        ),
         BottomNavigationBarItem(
-            icon: Icon(Icons.qr_code_2_outlined), label: 'QR'),
+          icon: Icon(Icons.calendar_month_outlined),
+          label: 'Events',
+        ),
         BottomNavigationBarItem(
-            icon: Icon(Icons.notifications_active_outlined),
-            label: 'Notifications'),
+          icon: Icon(Icons.qr_code_2_outlined),
+          label: 'QR',
+        ),
         BottomNavigationBarItem(
-            icon: Icon(Icons.person_2_outlined), label: 'Profile'),
+          icon: Icon(Icons.notifications_active_outlined),
+          label: 'Notifications',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_2_outlined),
+          label: 'Profile',
+        ),
       ],
     );
   }
