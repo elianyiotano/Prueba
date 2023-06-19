3645159f26c44d2d1527b1a6ac611155c89fc9ee
Ana Valverde <ana.valverde@4geeks.io>
2023-06-19 12:40:32 -0600

    feat(LE-4): add constanst

---
+++ lib/constants/constants.dart
@@ -0,0 +1,14 @@
+class ApiConstants {
+  static String baseUrl = 'http://localhost:3000';
+  static String singInEndpoint = '/api/auth/sign_in';
+  static String singUpEndpoint = '/api/auth/';
+  static String sendResetPasswordEmail = '/api/auth/password';
+
+  static String getListActivity = '/api/user/activity/all';
+  static String getUser = '/api/user/';
+  static String getMessage = '/api/user/message';
+
+  static String getEvents = '/api/event/all';
+
+  static String getRanking = '/api/ranking';
+}
