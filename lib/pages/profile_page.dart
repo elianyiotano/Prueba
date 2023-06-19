43251ca738f2f1e49200857685428149b9870e35
Ana Valverde <ana.valverde@4geeks.io>
2023-06-18 21:50:01 -0600

    feat(LE-4): basic structure profile user

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -4,17 +4,144 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/my_profile.jpg'),
-            fit: BoxFit.cover,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 50), // Espacio en blanco arriba
+          Center(
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 5,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+                image: const DecorationImage(
+                  image: AssetImage('assets/images/my_profile.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
           ),
-        ),
-        clipBehavior: Clip.antiAlias,
-        child: Container(
-          color: Colors.white,
-        ),
+          const SizedBox(height: 10),
+          Text(
+            'Viviana Morales',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              fontFamily: 'Poppins',
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            'Category: Silver',
+            style: TextStyle(
+              fontSize: 15,
+              fontFamily: 'Poppins',
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    '195',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Poppins',
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                  Text(
+                    'Events',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'Poppins',
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF525E7B),
+                      height: 1.4,
+                      letterSpacing: 0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(width: 20), // Espacio entre los bloques
+              Column(
+                children: [
+                  Text(
+                    '15',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Poppins',
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                  Text(
+                    'Points',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'Poppins',
+                      fontStyle: FontStyle.normal,
+                      color: Color(0xFF525E7B),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: Color(0xFFE5E5E5)),
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'My Points',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Poppins',
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Center(
+                    child: Text(
+                      'Contenido del card',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
