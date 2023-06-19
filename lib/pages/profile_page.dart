5b2df23888239c86a9d36490688d160f4913d423
Ana Valverde <ana.valverde@4geeks.io>
2023-06-18 23:44:28 -0600

    feat(LE-4): add complete structure page profile

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
@@ -6,8 +7,7 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 50), // Espacio en blanco arriba
+        children: [// Espacio en blanco arriba
           Center(
             child: Container(
               width: 100,
@@ -106,19 +106,20 @@ class ProfilePage extends StatelessWidget {
           ),
           const SizedBox(height: 20),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(8),
               border: Border.all(color: Color(0xFFE5E5E5)),
               color: Colors.white,
             ),
             child: Padding(
-              padding: EdgeInsets.all(10),
+              padding: EdgeInsets.all(0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  SizedBox(height: 2),
                   Text(
-                    'My Points',
+                    '   My Points',
                     style: TextStyle(
                       fontSize: 16,
                       fontFamily: 'Poppins',
@@ -126,19 +127,152 @@ class ProfilePage extends StatelessWidget {
                       fontWeight: FontWeight.w600,
                     ),
                   ),
-                  SizedBox(height: 10),
-                  Center(
-                    child: Text(
-                      'Contenido del card',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontFamily: 'Poppins',
-                        fontStyle: FontStyle.normal,
-                      ),
+                  SizedBox(height: 1),
+                  Container(
+                    height: 200,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SfRadialGauge(
+                          axes: <RadialAxis>[
+                            RadialAxis(
+                              minimum: 0,
+                              maximum: 100,
+                              showLabels: false,
+                              showTicks: false,
+                              startAngle: 150,
+                              endAngle: 30,
+                              radiusFactor: 0.8,
+                              axisLineStyle: AxisLineStyle(
+                                thickness: 20,
+                                color: Colors.grey[300],
+                                thicknessUnit: GaugeSizeUnit.logicalPixel,
+                              ),
+                              pointers: <GaugePointer>[
+                                RangePointer(
+                                  value: 75,
+                                  width: 20,
+                                  color: Colors.green,
+                                  pointerOffset: 10,
+                                  enableAnimation: true,
+                                  animationDuration: 1000,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Positioned.fill(
+                          child: FractionallySizedBox(
+                            alignment: Alignment.center,
+                            widthFactor: 0.4,
+                            heightFactor: 0.4,
+                            child: Container(
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Progreso',
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    '195/500',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontFamily: 'Poppins',
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
+            ),
+          ),
+          SizedBox(height: 10), // Espacio entre el contenedor del progreso y el texto "Recently activity"
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                'Recently activity',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'Poppins',
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10), // Espacio entre el texto "Recently activity" y la lista de actividades
+          Expanded(
+            child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/images/my_profile.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Activity Title',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 3),
+                            Text(
+                              'Activity Description',
+                              style: TextStyle(
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        '+10',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
           ),
         ],
