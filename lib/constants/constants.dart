b45635119582659759f416cf8674db5cdc1e3d95
Ana Valverde <ana.valverde@4geeks.io>
2023-07-03 10:20:11 -0600

    refactor: clean code

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,5 @@
 class ApiConstants {
-  //static String baseUrl = 'https://run.mocky.io/v3';
-  //static String singInEndpoint = '/a5e013c1-91db-43bf-8538-1bb4296fc056';
+
   static String baseUrl = 'https://loyalty.peppermind.io';
   static String singInEndpoint = '/auth/login';
   static String singUpEndpoint = '/auth/signup';
